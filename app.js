45b87af59c0ea80de3667b58b651052db2368fb7
Abhishek <abhishek@mactosys.com>
2024-01-12 13:00:54 +0530

    new

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 1330;
+
+app.get("/", function (req, res) {
+  res.json({
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail: "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+    images: [
+      "https://cdn.dummyjson.com/product-images/1/1.jpg",
+      "https://cdn.dummyjson.com/product-images/1/2.jpg",
+      "https://cdn.dummyjson.com/product-images/1/3.jpg",
+      "https://cdn.dummyjson.com/product-images/1/4.jpg",
+      "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+    ],
+  });
+});
+
+app.listen(port, function () {
+  console.log(`Server is running on ${port}`);
+});
